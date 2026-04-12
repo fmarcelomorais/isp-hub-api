e3eec4fb0ead6a94f2f4217cc95eed0984954ddf
fmarcelomorais <marcelomorais.program@gmail.com>
2026-04-12 11:44:45 -0300

    teste minimo 9

--- api/index.js
+++ api/index.js
@@ -1,3 +1,39 @@
-export default function handler(req, res) {
-  res.status(200).send('OK 🚀');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import pg from 'pg';
+import serverless from 'serverless-http';
+
+import router from '../routes/rotas.js'
+
+const { Pool } = pg;
+
+const app = express();
+
+// ✅ Pool GLOBAL (evita recriar conexão)
+let pool;
+
+if (!global.pool) {
+  global.pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
 }
+
+pool = global.pool;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+// ✅ Suas rotas principais
+app.use('/', router);
+
+// ❌ NÃO usar app.listen na Vercel
+
+// ✅ Export serverless
+export default serverless(app);
+
+// ✅ Export pool (uso nos repositories)
+export { pool };
