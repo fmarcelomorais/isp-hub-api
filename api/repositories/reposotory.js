c801880445e9b345b25ed0277e7db76ed0ae280d
fmarcelomorais <marcelomorais.program@gmail.com>
2026-04-12 10:24:23 -0300

    alterando caminhos

--- api/repositories/reposotory.js
+++ api/repositories/reposotory.js
@@ -1,4 +1,4 @@
-import { pool } from '../api/index.js';
+import { pool } from '../index.js';
 
 export async function getServer() {
     const sql = 'SELECT nome, servidor, ativo FROM servidores WHERE ativo = true';
